bdf31b2c10f8d2a8b941f3b918b02bf20cb6418e
Tahseen Anwar Tahir <37225838+tahseenat@users.noreply.github.com>
2020-02-14 20:01:07 +0530

    Added thumbnail API

--- newproj/app.py
+++ newproj/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, jsonify
 import jwt
+import urllib.request
+import base64
+import cv2
 
 JWT_SECRET = 'secret'
 JWT_ALGORITHM = 'HS256'
+
+img_name = "original.jpg"
+thumbnail_name = "thumb.jpg"
+
+width = 50
+height = 50
+dim = (width, height)
 
 # Init app
 app = Flask(__name__)
@@ -18,16 +28,34 @@ def login():
         'password': password
     }
     jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
-    token = {
+    dict = {
         'token': str(jwt_token)
     }
-    return token
+    return dict
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/image_thumbnail', methods=['POST'])
 def image_thumbnail():
-    token = request.json['token']
-    return token
+    # token = request.json['token']
+    image_url = request.json['image_url']
+
+    f = open(img_name, 'wb')
+    f.write(urllib.request.urlopen(image_url).read())
+    f.close()
+    image_name = cv2.imread(img_name, cv2.IMREAD_UNCHANGED)
+    thumbnail_image = cv2.resize(image_name, dim, interpolation=cv2.INTER_AREA)
+    cv2.imwrite("thumb.jpg", thumbnail_image)
+
+    string = base64.b64encode(cv2.imencode(thumbnail_name, thumbnail_image)[1]).decode()
+    dict = {
+        'thumbnail': string
+    }
+    return dict
+
+
+@app.route('/json_patching', methods=['POST'])
+def json_patching():
+    return "hello"
 
 
 if __name__ == '__main__':
