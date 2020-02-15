0a9aaad8d4345a94804ad14a12aa4c5200536512
Tahseen Anwar Tahir <37225838+tahseenat@users.noreply.github.com>
2020-02-15 20:12:31 +0530

    Added JWT validation in APIs

--- newproj/app.py
+++ newproj/app.py
@@ -3,6 +3,7 @@ import jwt
 import urllib.request
 import base64
 import cv2
+from functools import wraps
 
 JWT_SECRET = 'secret'
 JWT_ALGORITHM = 'HS256'
@@ -18,6 +19,21 @@ dim = (width, height)
 app = Flask(__name__)
 
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 403
+        try:
+            data = jwt.decode(token, JWT_SECRET)
+        except:
+            return jsonify({'message': 'Token is invalid'}), 403
+        return f(*args, **kwargs)
+
+    return decorated
+
+
 # login function
 @app.route('/login', methods=['POST'])
 def login():
@@ -25,50 +41,55 @@ def login():
         username = request.json['username']
         password = request.json['password']
     except:
-        return "Bad Request"
+        return jsonify({'message': 'username/password is missing'})
 
     if len(username) > 0 and len(password) > 0:
         payload = {
             'username': username,
             'password': password
         }
-        jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
-        dict = {
-            'token': str(jwt_token)
+        header = {
+            "alg": JWT_ALGORITHM,
+            "typ": "JWT"
         }
-        return dict
+        jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
+        return jsonify({'token': jwt_token.decode('UTF-8')})
     else:
-        return "Username/Password length invalid"
+        return jsonify({'message': 'username/password is Invalid'})
 
 
 @app.route('/image_thumbnail', methods=['POST'])
+@token_required
 def image_thumbnail():
     try:
-        token = request.json['token']
         image_url = request.json['image_url']
     except:
-        return "Bad Request"
+        return jsonify({'message': 'Token/image_url is missing'})
 
     try:
         f = open(img_name, 'wb')
         f.write(urllib.request.urlopen(image_url).read())
         f.close()
     except:
-        return "Invalid URL"
+        return jsonify({'message': 'Invalid URL'})
 
     image_name = cv2.imread(img_name, cv2.IMREAD_UNCHANGED)
     thumbnail_image = cv2.resize(image_name, dim, interpolation=cv2.INTER_AREA)
     cv2.imwrite("thumb.jpg", thumbnail_image)
 
     string = base64.b64encode(cv2.imencode(thumbnail_name, thumbnail_image)[1]).decode()
-    dict = {
+    message = {
+        'status': 'Success',
         'thumbnail': string
     }
-    return dict
+    return message
 
 
 @app.route('/json_patching', methods=['POST'])
+@token_required
 def json_patching():
+    data = request.json
+
     return "hello"
 
 
